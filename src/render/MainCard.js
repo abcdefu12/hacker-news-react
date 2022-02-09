972460301ae9b3fd907e8c178172a5246094ef1a
Gim <gbks081@gmail.com>
2022-02-10 01:11:18 +0900

    feat: main css finished

--- src/render/MainCard.js
+++ src/render/MainCard.js
@@ -7,15 +7,21 @@ import { BsChatText } from 'react-icons/bs';
 function MainCard({by, descendants, id, kids, score, text, time, title, color}){
 
     return(
+        // MAIN CARD COMMON
         <div key="mainCard">            
             <div className={styles.frame}>
-                <div className={styles.title}> {title} </div> 
+                {/* TITLE */}
+                <div className={styles.title}> {title} </div>
+                {/* TEXT(CONTENT) */}
                 <div className={styles.text}>
                     {text ? `${text.slice(0,245)}...` : null }
                 </div>
+                {/* SCORE AND DESCENDANTS */}
                 <div> <Heart color={color} > 
-                    <MdFavorite />&nbsp; {score} &nbsp;
-                    <BsChatText className={styles.text_icon}/> {descendants}
+                    <MdFavorite />&nbsp;
+                    {score} &nbsp;
+                    {descendants ? <BsChatText className={styles.text_icon}/> : null } &nbsp;
+                    {descendants? <>{descendants}</> : null }
                 </Heart> </div>
 
             </div>      
